8a78f2b0e0ed576453b21773531b0c284e47602b
Daniele Olivieri <danieledaniele1988@gmail.com>
2019-09-04 13:04:08 +0200

    Calcolo stadio a reazione

--- CPP/Esercizio_4/esercizio_4.cpp
+++ CPP/Esercizio_4/esercizio_4.cpp
@@ -5,9 +5,11 @@
 using namespace std;
 
 #define PI 3.14159265
+#define e 2.71828
 float P1,P2,T1,T2,v1,v2;
 float k = 1.4,R=0.287,Cp=1.005;
 float dHstar,c_1,c_2,alpha;
+float r, Tmedia,beta_parziale;
 
 void input_dati(){
     cout << "Inserisci la pressione di ristagno (bar): ";
@@ -33,8 +35,8 @@ float entalpia_disp(float t1,float t2){
     return Cp*(t1-t2);
 }
 
-float c1(float H){
-    return pow(2*H,0.5);
+float c1(float H,float r){
+    return pow(2*H*(1-r),0.5);
 }
 
 float u(float angolo,float c){
@@ -53,6 +55,10 @@ float work(float vel,float H){
     return H-(pow(vel,2)/2);
 }
 
+float eta_reazione(float angolo){
+    return 2*pow(cos(angolo*PI/180),2)/(1+pow(cos(angolo*PI/180),2));
+}
+
 int main(){
     input_dati();
     T2 = calcolo_T2(P1/P2,T1,k);
@@ -65,13 +71,24 @@ int main(){
     cout <<"2\t"<<P2<<'\t'<<setprecision(pr)<<v2<<"\t  "<<T2-273.15 <<endl<<endl;
     dHstar = entalpia_disp(T1,T2);
     cout << "L'entalpia disponibile è: "<< dHstar << " kJ/kg\n";
-    c_1 = c1(dHstar);
+    c_1 = c1(dHstar,0);
     cout << "Velocità c_1 per stadio ad azione: " << c_1 << " m/s\n";
     cout << "Velocità tangenziale u del rotore: " << u(alpha,c_1) << " m/s\n";
     c_2 = vel_uscita(alpha,c_1);
     cout << "Velocità assiale in uscita c_2: " << c_2 << " m/s\n";
     cout << "Lavoro ottenuto: " << work(c_2,dHstar) << " kJ/kg\n";
-    cout << "Rendimento massimo a " << alpha << "° : " << eta_max(alpha) << endl;
+    cout << "Rendimento massimo a " << alpha << "° : " << eta_max(alpha) << endl<<endl;
     //cout << "Rendimento effettivo: " << work(c_2,dHstar)/dHstar << endl;
+    cout << "Calcolo stadio a reazione!\n\n";
+    cout << "Inserisci il grado di reazione (0~1): ";
+    cin >> r;
+    c_1 = c1(dHstar,r);
+    cout << "Velocità c_1 per stadio a reazione: " << c_1 << " m/s\n";
+    cout << "Velocità tangenziale u del rotore: " << u(alpha,c_1)*2 << " m/s\n";
+    cout << "Rendimento massimo a " << alpha << "° : " << eta_reazione(alpha) << endl<<endl;
+    Tmedia = T1-(pow(c_1,2)/(2*Cp));
+    cout << "Temperatura intermedia: " << Tmedia-273.15 <<" °C\n";
+    beta_parziale = pow(e,Cp/R*log(T1/Tmedia));
+    cout << "Pressione parziale: " << P1/beta_parziale << " bar\n";
     return 0;
 }
